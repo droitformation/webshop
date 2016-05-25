3a9691807ae8351e008c863988eb1597bc10f9f7
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-05-25 10:26:40 +0200

    fix title missing from newsletter imagettext content in send partial

--- resources/views/backend/newsletter/send/imageText.blade.php
+++ resources/views/backend/newsletter/send/imageText.blade.php
@@ -16,6 +16,9 @@
                                 <img style="max-width: 560px;" alt="Droit du travail" src="{{ asset('files/uploads/'.$bloc->image) }}" />
                             </a>
                         </p>
+                        @if(!empty($bloc->titre))
+                            <h2 style="font-family: sans-serif;">{{ $bloc->titre }}</h2>
+                        @endif
                         <div>{!! $bloc->contenu !!}</div>
                     </td>
                 </tr>
