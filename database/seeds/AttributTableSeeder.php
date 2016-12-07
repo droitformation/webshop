e4a1d9cf826311b93df0740d4eefa9f6fe6c3668
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-12-07 19:30:47 +0100

    fix seeder attributes

--- database/seeds/AttributTableSeeder.php
+++ database/seeds/AttributTableSeeder.php
@@ -8,10 +8,10 @@ class AttributTableSeeder extends \Illuminate\Database\Seeder  {
 		DB::table('shop_attributes')->truncate();
 
 		$shop_attributes = array(
-			array('id' => '1','title' => 'ISBN','reminder' => NULL,'text' => NULL,'intervals' => '','deleted_at' => NULL),
-			array('id' => '2','title' => 'ISSN','reminder' => NULL,'text' => NULL,'intervals' => '','deleted_at' => NULL),
-			array('id' => '3','title' => 'Référence','reminder' => NULL,'text' => NULL,'intervals' => '','deleted_at' => NULL),
-			array('id' => '4','title' => 'Édition','reminder' => NULL,'text' => NULL,'intervals' => '','deleted_at' => NULL)
+			array('id' => '1','title' => 'ISBN','reminder' => NULL,'text' => NULL,'duration' => '','deleted_at' => NULL),
+			array('id' => '2','title' => 'ISSN','reminder' => NULL,'text' => NULL,'duration' => '','deleted_at' => NULL),
+			array('id' => '3','title' => 'Référence','reminder' => NULL,'text' => NULL,'duration' => '','deleted_at' => NULL),
+			array('id' => '4','title' => 'Édition','reminder' => NULL,'text' => NULL,'duration' => '','deleted_at' => NULL)
 		);
 
 		// Uncomment the below to run the seeder
