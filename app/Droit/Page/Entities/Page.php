ac75b7e822f694a15b33dd2ff48b0abfc1620e7a
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-02-21 16:22:19 +0100

    page contents rang

--- app/Droit/Page/Entities/Page.php
+++ app/Droit/Page/Entities/Page.php
@@ -93,7 +93,7 @@ class Page extends Node {
 
     public function contents()
     {
-        return $this->hasMany('App\Droit\Content\Entities\Content');
+        return $this->hasMany('App\Droit\Content\Entities\Content')->orderBy('rang','ASC');
     }
 
     public function blocs()
