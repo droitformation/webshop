e5c901e5aba715ca55da182eebfa7bf5846baf69
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-02-24 11:24:47 +0100

    fix logos sites

--- config/sites.php
+++ config/sites.php
@@ -39,8 +39,8 @@ return [
     ],
     'fac_sites' => [
         'pubdroit'         => ['image' => 'pubdroit.png', 'url' => 'http://www.publications-droit.ch'],
-        'bail'             => ['image' => 'bail.png', 'url' => 'http://www.droitmatrimonial.ch'],
-        'matrimonial'      => ['image' => 'matrimonial.png', 'url' => 'http://www.bail.ch'],
+        'bail'             => ['image' => 'bail.png', 'url' => 'http://www.bail.ch'],
+        'matrimonial'      => ['image' => 'matrimonial.png', 'url' => 'http://www.droitmatrimonial.ch'],
         'droitpraticien'   => ['image' => 'droitpraticien.png', 'url' => 'http://www.droitpraticien.ch'],
         'tribunauxcivils'  => ['image' => 'tribunaux.png', 'url' => 'http://www.tribunauxcivils.ch'],
         'droitenschemas'   => ['image' => 'schemas.png', 'url' => 'http://www.droitenschemas.ch'],
