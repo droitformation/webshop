108eaef17d0030326dc6b1294e79eb8259e227d8
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-03-14 13:54:38 +0100

    fix bug small width with jurisprudence

--- public/frontend/common/js/sidebar.js
+++ public/frontend/common/js/sidebar.js
@@ -21,21 +21,26 @@ $(function() {
         return this.each(function () {
 
             // default css declaration
-            var elem = $(this).css('position', 'fixed');
+            var width = $('#appComponent').width();
+            
+            if(width > 860){
+                var elem = $(this).css('position', 'fixed');
 
-            var setPosition = function () {
-                var top = 0;
-                // get no of pixels hidden above the the window
-                var scrollTop = $(window).scrollTop();
-                // get elements distance from top of window
-                var topBuffer = ((settings.topBoundary || 0) - scrollTop);
-                // update position if required
-                if (topBuffer >= 0) { top += topBuffer }
-                elem.css('top', top);
-            };
+                var setPosition = function () {
+                    var top = 0;
+                    // get no of pixels hidden above the the window
+                    var scrollTop = $(window).scrollTop();
+                    // get elements distance from top of window
+                    var topBuffer = ((settings.topBoundary || 0) - scrollTop);
+                    // update position if required
+                    if (topBuffer >= 0) { top += topBuffer }
+                    elem.css('top', top);
+                };
 
-            $(window).bind('scroll', setPosition);
-            setPosition();
+                $(window).bind('scroll', setPosition);
+                setPosition();
+            }
+
         });
     };
 
