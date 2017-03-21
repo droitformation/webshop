ba1b9a6410bbe18176a527fc1c606ec60d683737
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-03-21 13:33:13 +0100

    analyses in group newsletter

--- resources/views/frontend/newsletter/content/groupe.blade.php
+++ resources/views/frontend/newsletter/content/groupe.blade.php
@@ -35,6 +35,48 @@
             </div>
             <div class="clear"></div>
         </div>
+        
+        @if(!$arret->analyses->isEmpty())
+            @foreach($arret->analyses as $analyse)
+                <div class="row">
+                    <div class="col-md-9">
+                        <h4>Commentaire l'arrêt {{ $arret->reference }}</h4>
+
+                        @if(!$analyse->authors->isEmpty())
+                            @foreach($analyse->authors as $author)
+
+                                <div class="media">
+                                    @if(File::exists(config('newsletter.path.author').$author->author_photo))
+                                        <div class="media-left">
+                                            <a href="#" style="display: block; width: 65px; margin-bottom: 10px;">
+                                                <img class="media-object" alt="{{ $author->name }}" src="{{ secure_asset(config('newsletter.path.author').$author->author_photo) }}">
+                                            </a>
+                                        </div>
+                                    @endif
+                                    <div class="media-body">
+                                        <h4><i>{{ $author->name }}</i></h4>
+                                        <p>{{ $author->occupation }}</p>
+                                    </div>
+                                </div>
+
+                            @endforeach
+                        @endif
+
+                        {!! $analyse->abstract !!}
+                        <p><a href="{{ secure_asset(config('newsletter.path.analyse').$analyse->file) }}">Télécharger en pdf</a></p>
+                    </div>
+                    <div class="col-md-3">
+                        <a target="_blank" href="{{ config('newsletter.link.analyse') }}">
+                            <?php $slug = $campagne->newsletter->site_id ? '/'.$campagne->newsletter->site->slug.'/' : ''; ?>
+                            <img border="0" style="max-width: 130px;" alt="Analyses" src="{{ secure_asset(config('newsletter.path.categorie').$slug.'analyse.jpg') }}">
+                        </a>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            @endforeach
+        @endif
+
+        
     @endforeach
 @endif
 
