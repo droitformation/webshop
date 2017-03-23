f8eb2fbb22e0d86e2368edb1e161c22c93f78911
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-03-23 08:07:33 +0100

    add more rappels with button

--- resources/views/backend/inscriptions/rappels/index.blade.php
+++ resources/views/backend/inscriptions/rappels/index.blade.php
@@ -13,21 +13,27 @@
                         <h3 style="margin-bottom:0px;line-height:20px;font-size: 18px;"><a href="{{ url('admin/colloque/'.$colloque->id) }}">{{ $colloque->titre }}</a></h3>
                         <p style="margin-bottom: 0;">{{ $colloque->event_date }}</p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-4" style="border-right: 1px solid #cecece;">
+                    <form action="{{ url('admin/inscription/rappel/make') }}" method="POST" class="form-inline">
+                        {!! csrf_field() !!}
+                        <input type="hidden" name="colloque_id" value="{{ $colloque->id }}">
+                        <div class="checkbox text-right" style="border-left: 1px solid #cecece; padding-left: 10px;">
+                            <label>
+                                <input name="more" value="1" type="checkbox"> &nbsp;<strong>Ajouter 1 rappel</strong> <br/>aux rappels existants
+                            </label>
+                        </div>
+                        <button class="btn btn-brown pull-right" type="submit">
+                            <i class="fa fa-bell"></i> &nbsp;Générer tous les rappels
+                        </button>
+                    </form>
+                </div>
+                <div class="col-md-2">
                     <form action="{{ url('admin/inscription/rappel/send') }}" method="POST" class="pull-right" style="margin-left: 20px;">
                         {!! csrf_field() !!}
                         <input type="hidden" name="colloque_id" value="{{ $colloque->id }}">
                         <input type="hidden" name="count" value="{{ !$inscriptions->isEmpty() ? $inscriptions->count() : 0 }}">
                         <button class="btn btn-inverse pull-right" id="confirmSendRappels">
                             <i class="fa fa-paper-plane"></i> &nbsp;Envoyer les rappel
-                        </button>
-                    </form>
-
-                    <form action="{{ url('admin/inscription/rappel/make') }}" method="POST" class="pull-right">
-                        {!! csrf_field() !!}
-                        <input type="hidden" name="colloque_id" value="{{ $colloque->id }}">
-                        <button class="btn btn-brown pull-right" type="submit">
-                            <i class="fa fa-bell"></i> &nbsp;Générer tous les rappels
                         </button>
                     </form>
                 </div>
