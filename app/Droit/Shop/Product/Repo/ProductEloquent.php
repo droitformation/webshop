ab555fa7ebd056f7a827d232a9347b2fc130431b
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-04-10 08:45:05 +0200

    change order categories for revue in bail

--- app/Droit/Shop/Product/Repo/ProductEloquent.php
+++ app/Droit/Shop/Product/Repo/ProductEloquent.php
@@ -63,7 +63,7 @@ class ProductEloquent implements ProductInterface{
                 $query->where('shop_categories.id', '=' ,$id);
                 $query->orWhere('shop_categories.title', 'LIKE' ,'%'.$id.'%');
             })
-            ->orderBy('created_at', 'DESC')
+            ->orderBy('created_at', 'ASC')
             ->get();
     }
 
