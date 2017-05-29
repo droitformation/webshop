9a9ee83d1c99ee55ba798d5ef7ed9146f8f308b9
Cristian <cristian.ferrara@sunrise.ch>
2017-05-29 11:53:48 +0200

    Ajout d'une catégorie de questions

--- resources/views/sondages/create.blade.php
+++ resources/views/sondages/create.blade.php
@@ -1,7 +1,27 @@
 @extends('sondages.layouts.master')
 @section('content')
 
-
+<!-- section class="row">
+<!-- *************************************
+Présentation du colloque avec illustration
+******************************************
+	   
+		<div class="col-md-12">
+		
+				<div class="col-md-3">
+					<div class="b-img-holder">
+					<img src="https://www.publications-droit.ch/files/colloques/illustration/2eeeb5b32fbca0360ab48808e0ba642e.JPG" width="195"></img>
+					</div>			
+				</div>
+				
+				<div class="col-md-9">
+				
+							<h4><strong class="title">Formulaire d'évaluation</strong></h4>
+							<h2>Les droits d’emption, de préemption et de réméré</h2><br>
+							<strong>vendredi 02 juin 2017</strong>
+						</div>
+				</div>
+</section -->
     <h3>Sondage pour {{ $sondage->colloque->titre }}</h3>
 
     <form class="form-sondage" action="{{ url('reponse') }}" method="POST">{!! csrf_field() !!}
@@ -16,6 +36,8 @@
 
                     @if($avis->type == 'text')
                         <textarea class="form-control form-control-sondage" name="reponses[{{ $avis->id }}]"></textarea>
+					@else if($avis->type == 'chapitre')
+						<h4><strong>{!! strip_tags($avis->question) !!}</strong></h4>
                     @else
                         <?php $choices = explode(',', $avis->choices); ?>
                         @foreach($choices as $choices)
@@ -30,6 +52,7 @@
                 </div>
             @endforeach
             <hr/>
+			<h3> <strong>Merci d'avoir participer.</strong></h3> 
             <button type="submit" class="btn btn-primary">Envoyer le sondage</button>
 
         @endif
