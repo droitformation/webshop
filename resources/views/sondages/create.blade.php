e7011ddbb83e0b843f311a3bff202a7be9b85aef
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-08-21 09:46:20 +0200

    change git add -p

--- resources/views/sondages/create.blade.php
+++ resources/views/sondages/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     @if($isTest)
-       <div class="alert alert-warning">Ceci est un sondage test</div>
+        <div class="alert alert-warning">Ceci est un sondage test</div>
     @endif
 
     @if($sondage->marketing)
@@ -21,29 +21,37 @@
 
         @if(!$sondage->avis->isEmpty())
             @foreach($sondage->avis as $avis)
-                <div class="form-group form-group-sondage">
-                    <label for="message" class="control-label label-question"><strong>{!! strip_tags($avis->question) !!}</strong></label>
 
-                    @if($avis->type == 'text')
-                        <textarea class="form-control form-control-sondage" name="reponses[{{ $avis->id }}]"></textarea>
-					@elseif($avis->type == 'chapitre')
-						<h4><strong>{!! strip_tags($avis->question) !!}</strong></h4>
-                    @else
-                        <?php $choices = explode(',', $avis->choices); ?>
-                        @foreach($choices as $choices)
-                            <div class="{{ $avis->type }}">
-                                <label>
-                                    <input type="{{ $avis->type }}" name="reponses[{{ $avis->id }}]" value="{{ $choices }}">{{ $choices }}
-                                </label>
-                            </div>
-                        @endforeach
+                <div class="form-group-sondage">
+                    @if($avis->type == 'chapitre')
+                        <h4 class="label-chapitre"><strong>{!! strip_tags($avis->question) !!}</strong></h4>
+
+                    @elseif($avis->type == 'text')
+                        <label for="message" class="control-label">Remarques :</label>
+                        <textarea class="form-control sondage" name="reponses[{{ $avis->id }}]"></textarea>
+
+                    @else($avis->type == 'question')
+                        <ul>
+                            <li class="sondage-question"><label for="message" class="control-label label-question">{!! strip_tags($avis->question) !!}</li>
+                        </ul>
+                        <ul>
+                            <?php $choices = explode(',', $avis->choices); ?>
+                            @foreach($choices as $choices)
+                                <li class="radio-sondage">
+                                    <input class="sondage" type="{{ $avis->type }}" name="reponses[{{ $avis->id }}]" value="{{ $choices }}">{{ $choices }}
+                                </li>
+
+                            @endforeach
+                        </ul>
                     @endif
 
                 </div>
             @endforeach
-            <hr/>
-			<h3> <strong>Merci d'avoir participer.</strong></h3> 
-            <button type="submit" class="btn btn-primary">Envoyer le sondage</button>
+            <hr class="sondage"/>
+            <div class="remerciements sondage">
+                <h3> <strong>Merci d'avoir participé</strong></h3>
+                <button type="submit" class="btn btn-primary">Envoyer le sondage</button>
+            </div>
 
         @endif
 
