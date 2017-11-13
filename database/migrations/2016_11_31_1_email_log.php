405dbc94ebe4f2c00ba5daeba60eba9b18ad64a1
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-11-13 15:41:31 +0100

    change email log migration class name

--- database/migrations/2016_11_31_1_email_log.php
+++ database/migrations/2016_11_31_1_email_log.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EmailLog extends Migration
+class CreateEmailLogTable extends Migration
 {
     /**
      * Run the migrations.
