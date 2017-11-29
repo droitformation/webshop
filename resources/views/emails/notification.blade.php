c5a0d4f85f7917d7dc0e7ccee1c6ab934937d320
Cindy Leschaud <droitformation.web@gmail.com>
2017-11-29 09:36:33 +0100

    re add links to notification for admin

--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -1,7 +1,7 @@
 @extends('emails.layouts.notification')
 @section('content')
 
-    <a style="{{ $fontFamily }} display:block; height: 115px;" href="#" target="_blank">
+    <a style="{{ $fontFamily }} display:block; height: 115px;" href="{{ url('/pubdroit') }}" target="_blank">
         <img width="max-width:100%;" src="{{ secure_asset('images/pubdroit/header_email.png') }}" alt="{{ config('app.name') }}">
     </a>
     <table style="{{ $style['email-body_inner'] }}" align="center" width="570" cellpadding="0" cellspacing="0">
@@ -25,7 +25,7 @@
                         </ul>
                     @endif
                 </div>
-
+                <p style="{{ $style['paragraph'] }}"><a href="{{ $link }}">Voir {{ $what }}</a></p>
             </td>
         </tr>
     </table>
