910155f3f2db6e912df112701dd9946fec337afc
Cindy Leschaud <droitformation.web@gmail.com>
2018-01-09 16:11:22 +0100

    add dev

--- routes/dev.php
+++ routes/dev.php
@@ -1115,7 +1115,7 @@ Route::get('/test_mailgun', function () {
 
 Route::get('factory', function()
 {
-<<<<<<< HEAD
+
 
     $model  = \App::make('App\Droit\Shop\Order\Repo\OrderInterface');
     $product  = factory(App\Droit\Shop\Product\Entities\Product::class)->make(['weight' => 10000, 'price'  => 1000]);
@@ -1141,7 +1141,7 @@ Route::get('factory', function()
 
     echo '<pre>';
     print_r($paquets);
-=======
+
     $worker = \App::make('App\Droit\Newsletter\Worker\SubscriptionWorkerInterface');
     $subscribe = \App::make('App\Droit\Newsletter\Repo\NewsletterUserInterface');
     $mailjet =  \App::make('App\Droit\Newsletter\Worker\MailjetServiceInterface');
@@ -1165,15 +1165,12 @@ Route::get('factory', function()
     echo '</pre>';exit();
 */
 
-<<<<<<< HEAD
-=======
     $results = \Excel::load('public/files/import/pi2.xlsx', function($reader) {
         $reader->ignoreEmpty();
         $reader->setSeparator('\r\n');
     })->get();
 
     $results = $results->pluck('email');
->>>>>>> 068b722... tag add newsletter correspondant
 
     foreach($results as $email){
         $exist = $worker->exist($email);
