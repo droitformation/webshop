79ab641e5a0692962bb1cc7a6905da78994b508c
Cindy Leschaud <droitformation.web@gmail.com>
2018-01-16 15:38:30 +0100

    add email column in abo and organisateur for factures

--- app/Droit/Abo/Entities/Abo.php
+++ app/Droit/Abo/Entities/Abo.php
@@ -9,7 +9,7 @@ class Abo extends Model{
 
     protected $table = 'abos';
 
-    protected $fillable = ['title','plan','logo','name','compte','adresse','price','shipping','remarque'];
+    protected $fillable = ['title','plan','logo','email','name','compte','adresse','price','shipping','remarque'];
 
     public function getPlanFrAttribute()
     {
