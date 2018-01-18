b7eeaa997342d811145815c01f71212a82cb92cd
Cindy Leschaud <cindyleschaud@MacBook-Pro-de-Cindy-2.local>
2018-01-18 09:51:57 +0100

    unsub link

--- resources/views/emails/newsletter/layouts/master.blade.php
+++ resources/views/emails/newsletter/layouts/master.blade.php
@@ -75,9 +75,9 @@
                             <tr><td height="15"></td></tr><!-- space -->
                             <tr>
                                 <td align="center" class="linkGrey">Si vous ne désirez plus recevoir cette newsletter, vous pouvez vous désinscrire à tout moment en
-                                    <a href="[[UNSUB_LINK_EN]]"></a>
+
                                     <?php $site = isset($campagne->newsletter->site) ? $campagne->newsletter->preview.'/'.$campagne->newsletter->site->slug : 'pubdroit'; ?>
-                                    <a class="linkGrey" href="{{ url($site.'/unsubscribe') }}">cliquant ici</a>.
+                                    <a class="linkGrey" href="[[UNSUB_LINK_EN]]">cliquant ici</a>.
                                 </td>
                             </tr>
                             <tr><td height="15"></td></tr><!-- space bottom -->
