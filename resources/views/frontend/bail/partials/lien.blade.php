50d3f8e09c478e44e017739cc768c64823fc741a
Cindy Leschaud <droitformation.web@gmail.com>
2018-02-07 09:24:13 +0100

    fix bail page contents display

--- resources/views/frontend/bail/partials/lien.blade.php
+++ resources/views/frontend/bail/partials/lien.blade.php
@@ -1,6 +1,6 @@
-<div class="row">
-    @foreach($contents as $bloc)
+@foreach($contents as $bloc)
+    <div class="row">
         <div class="col-md-4"><p><strong>{!! $bloc->title !!}</strong></p></div>
         <div class="col-md-8">{!! $bloc->content !!}</div>
-    @endforeach
-</div>
+    </div>
+@endforeach
