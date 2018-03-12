79e235da8c0159332848590ca7e6f22541558750
Cindy Leschaud <droitformation.web@gmail.com>
2018-03-12 13:18:53 +0100

    bounce error email fix event

--- app/Http/Controllers/Backend/Newsletter/TrackingController.php
+++ app/Http/Controllers/Backend/Newsletter/TrackingController.php
@@ -73,10 +73,10 @@ class TrackingController extends Controller
     public function bounce(Request $request)
     {
         if(env('SEND_ADMIN')){
-            \Mail::to('droit.formation@unine.ch')->send(new \App\Mail\NotifyBounce($request->input('recipient'), $request->input('error'), []));
+            \Mail::to('droit.formation@unine.ch')->send(new \App\Mail\NotifyBounce($request->input('recipient'), $request->input('event'), []));
         }
 
-        \Mail::to('cindy.leschaud@gmail.com')->send(new \App\Mail\NotifyBounce($request->input('recipient'), $request->input('error'), $request->all()));
+        \Mail::to('cindy.leschaud@gmail.com')->send(new \App\Mail\NotifyBounce($request->input('recipient'), $request->input('event'), $request->all()));
 
         //\Log::info(json_encode($request->all()));
     }
