322044d735c5a3a31bb23af4d9c882ec768ae0a5
Cindy Leschaud <droitformation.web@gmail.com>
2018-03-20 15:23:37 +0100

    add polyfill

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,7 +4,7 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
+require('es6-promise/auto');
 require('./bootstrap');
 
 window.Vue = require('vue');
