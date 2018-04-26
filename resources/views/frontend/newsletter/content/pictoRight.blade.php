3ca29f15ac0adb4843af1cb4a5fccf1113c5ec1e
Cindy Leschaud <cindyleschaud@MacBook-Pro-de-Cindy-2.local>
2018-04-26 08:56:33 +0200

    add content pictoRight.blade.php

---
+++ resources/views/frontend/newsletter/content/pictoRight.blade.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-md-9">
+        <div class="bloc-content">
+            <h2>{{ $bloc->titre }}</h2>
+            {!! $bloc->contenu !!}
+        </div><!--END POST-->
+    </div>
+    <div class="col-md-3">
+        <a target="_blank" href="{{ isset($bloc->lien) && !empty($bloc->lien) ? $bloc->link_or_url : url('/') }}">
+            <img width="130px" style="max-width: 130px; max-height: 220px;" alt="{{ $bloc->titre or '' }}" src="{{ secure_asset(config('newsletter.path.categorie').$bloc->image.'') }}" />
+        </a>
+    </div>
+</div>
