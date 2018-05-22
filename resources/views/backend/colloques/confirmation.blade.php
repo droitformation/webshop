eda6e070e12a959b90e83536c37684b8012158ae
Cindy Leschaud <droitformation.web@gmail.com>
2018-05-22 13:31:49 +0200

    style confirmation send slides page

--- resources/views/backend/colloques/confirmation.blade.php
+++ resources/views/backend/colloques/confirmation.blade.php
@@ -4,22 +4,42 @@
     <p><a href="{{ url('admin/colloque/'.$colloque->id) }}" class="btn btn-default"><i class="fa fa-arrow-left"></i> &nbsp;Retour au colloque</a></p>
 
     <div class="row">
+        <div class="col-md-3">
+            <h3 class="text-info">Confirmer et envoyer les liens aux emails</h3>
+        </div>
+    </div>
+
+    <div class="row">
         <div class="col-md-8">
 
             <div class="panel panel-default">
                 <div class="panel-body">
 
-                    <h3 class="text-info">Confirmer et envoyer les liens aux emails</h3>
+                    <h3>Colloque:</h3>
 
                     @if(!$emails->isEmpty())
-                        @foreach($emails as $email)
-                            <p>{{ $email }}</p>
-                        @endforeach
+                        <div class="well well-sm" style="padding: 5px 10px;">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <h3 style="margin-bottom: 0;"><strong>{{ $colloque->titre }}</strong></h3>
+                                </div>
+                                <div class="col-md-4 text-right">
+                                    <form action="{{ url('admin/slide/send') }}" method="POST">{!! csrf_field() !!}
+                                        <input type="hidden" name="colloque_id" value="{{ $colloque->id }}">
+                                        <button class="btn btn-primary">Envoyer les liens</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
 
-                        <form action="{{ url('admin/slide/send') }}" method="POST">{!! csrf_field() !!}
-                            <input type="hidden" name="colloque_id" value="{{ $colloque->id }}">
-                            <button class="btn btn-inverse btn-sm">Envoyer</button>
-                        </form>
+                        <h3>Emails:</h3>
+
+                        <ul>
+                            @foreach($emails as $email)
+                                <li>{{ $email }}</li>
+                            @endforeach
+                        </ul>
+
                     @else
                         <p class="text-danger"><i class="fa fa-exclamation-triangle"></i> &nbsp;Aucun email dans cette liste</p>
                     @endif
