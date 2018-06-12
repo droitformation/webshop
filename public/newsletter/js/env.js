faac24d643f59ec7c95e426a7ed1c1e3452901d2
Cindy Leschaud <droitformation.web@gmail.com>
2018-06-12 08:47:45 +0200

    remove cached files ignored

--- public/newsletter/js/env.js
+++
@@ -1,9 +0,0 @@
-(function () {
-
-    window.__env = window.__env || {};
-    // API url
-    window.__env.adminUrl = 'http://shop.local/admin/';
-
-    // Base url
-    window.__env.ajaxUrl = 'http://shop.local/admin/ajax/';
-}());
