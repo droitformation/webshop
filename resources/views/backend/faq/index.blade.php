b2b1bb3d0be1cd89d1adca340304b8cacbeb079a
Cindy Leschaud <droitformation.web@gmail.com>
2018-08-27 11:32:30 +0200

    change site config add faq

--- resources/views/backend/faq/index.blade.php
+++ resources/views/backend/faq/index.blade.php
@@ -7,67 +7,55 @@
     </div>
 </div>
 
-<?php $grouped_cats = $cats->groupBy('site_id'); ?>
+<?php $site = $sites->find($current_site); ?>
 
-@if(!$sites->isEmpty())
-    @foreach($sites as $site)
-        <div class="row">
-            <div class="col-md-12">
+<h3>FAQ</h3>
 
-                <p><img height="40" src="{{ secure_asset('logos/'.$site->logo) }}" alt="logo"></p>
-                <div class="panel panel-primary">
-                    <div class="panel-body">
+@if(!$cats->isEmpty())
+    <div class="row">
+        <div class="col-md-8">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    @foreach($cats as $categorie)
 
-                        @if(isset($grouped_cats[$site->id]))
-                            <?php $chunks = $grouped_cats[$site->id]->chunk(3); ?>
-                            @foreach($chunks as $chunk)
-                                <div class="row" style="margin-bottom: 15px;">
-                                    @foreach($chunk as $categorie)
-                                        <div class="col-md-4">
-                                            <h5>
-                                                <strong>{{ $categorie->title }}</strong>&nbsp;
-                                                <a href="{{ url('admin/question/create/'.$categorie->id) }}" class="btn btn-success btn-xs pull-right">Ajouter une question</a>
-                                            </h5>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <h4 style="margin-top: 0;"><strong>{{ $categorie->title }}</strong></h4>
+                            </div>
+                            <div class="col-md-4">
+                                <form action="{{ url('admin/faq/'.$categorie->id) }}" method="POST" class="form-horizontal text-right" style="margin-bottom: 20px;">
+                                    <input type="hidden" name="_method" value="DELETE">{!! csrf_field() !!}
+                                    <a href="{{ url('admin/question/create/'.$categorie->id) }}" class="btn btn-success btn-xs">Ajouter une question</a>
+                                    <a href="{{ url('admin/faq/'.$categorie->id) }}" class="btn btn-info btn-xs"><i class="fa fa-edit"></i></a>
+                                    <button data-what="Supprimer" data-action="{{ $categorie->title }}" class="btn btn-xs btn-danger btn-delete deleteAction">x</button>
+                                </form>
+                            </div>
+                        </div>
 
-                                            @if(!$categorie->questions->isEmpty())
-                                                <div class="list-group" style="margin-bottom: 5px;">
-                                                    @foreach($categorie->questions as $question)
-                                                        <div class="list-group-item">
-                                                            <div class="row">
-                                                                <div class="col-md-9">{{ $question->title }}</div>
-                                                                <div class="col-md-3 text-right">
-                                                                    <form action="{{ url('admin/question/'.$question->id) }}" method="POST">
-                                                                        <input type="hidden" name="_method" value="DELETE">{!! csrf_field() !!}
-                                                                        <a href="{{ url('admin/question/'.$question->id) }}" class="btn btn-info btn-xs"><i class="fa fa-edit"></i></a>
-                                                                        <button data-what="Supprimer" data-action="{{ $question->title }}" class="btn btn-xs btn-danger btn-delete deleteAction">X</button>
-                                                                    </form>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    @endforeach
-                                                </div>
-                                            @endif
-
-                                            <form action="{{ url('admin/faq/'.$categorie->id) }}" method="POST" class="form-horizontal text-right">
-                                                <input type="hidden" name="_method" value="DELETE">{!! csrf_field() !!}
-                                                <a href="{{ url('admin/faq/'.$categorie->id) }}" class="btn btn-info btn-xs">Editer la catégorie</a>
-                                                <button data-what="Supprimer" data-action="{{ $categorie->title }}" class="btn btn-xs btn-danger btn-delete deleteAction">Supprimer la catégorie</button>
-                                            </form>
-
+                        @if(!$categorie->questions->isEmpty())
+                            <div class="list-group" style="margin:0 5px 5px 5px;">
+                                @foreach($categorie->questions as $question)
+                                    <div class="list-group-item">
+                                        <div class="row">
+                                            <div class="col-md-9">{{ $question->title }}</div>
+                                            <div class="col-md-3 text-right">
+                                                <form action="{{ url('admin/question/'.$question->id) }}" method="POST">
+                                                    <input type="hidden" name="_method" value="DELETE">{!! csrf_field() !!}
+                                                    <a href="{{ url('admin/question/'.$question->id) }}" class="btn btn-info btn-xs"><i class="fa fa-edit"></i></a>
+                                                    <button data-what="Supprimer" data-action="{{ $question->title }}" class="btn btn-xs btn-danger btn-delete deleteAction">X</button>
+                                                </form>
+                                            </div>
                                         </div>
-                                    @endforeach
-                                </div>
-                            @endforeach
+                                    </div>
+                                @endforeach
+                            </div><br/><br/>
                         @endif
-
-                    </div>
+                    @endforeach
                 </div>
-
             </div>
         </div>
-    @endforeach
+    </div>
 @endif
-
 
 
 @stop
