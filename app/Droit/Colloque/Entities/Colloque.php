a29cd03aa7c03d1b82d452ccd91a76a4773e72d5
Cindy Leschaud <droitformation.web@gmail.com>
2018-11-13 11:35:57 +0100

    add linkedin metas

--- app/Droit/Colloque/Entities/Colloque.php
+++ app/Droit/Colloque/Entities/Colloque.php
@@ -129,7 +129,8 @@ class Colloque extends Model implements HasMedia
 
     public function getDescriptionAttribute()
     {
-        $html = !empty($this->soustitre) ? $this->soustitre.' | ' : '';
+        $html  = $this->titre.', ';
+        $html .= !empty($this->soustitre) ? $this->soustitre.' | ' : '';
         $html .= $this->location ? $this->location->name.' | ' : '';
         $html .= $this->event_date;
 
