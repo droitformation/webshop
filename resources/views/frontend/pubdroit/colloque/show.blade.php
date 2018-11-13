91f510f4b5f7c28a55b2879338f2718b0c823d6e
Cindy Leschaud <droitformation.web@gmail.com>
2018-11-13 11:23:46 +0100

    add linkedin test 2

--- resources/views/frontend/pubdroit/colloque/show.blade.php
+++ resources/views/frontend/pubdroit/colloque/show.blade.php
@@ -94,7 +94,7 @@
                         @endif
 
                         <div style="display: none;">
-                            <a href="http://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(url()->current()) }}&title={url_encoded_title}">Share on LinkedIn</a>
+                            <a href="http://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(url()->current()) }}&title=Colloque">Share on LinkedIn</a>
                             <a href="https://www.linkedin.com/shareArticle?mini=true&summary=Colloque&title=f1&url={{ urlencode('https://publications-droit.ch/pubdroit/colloque/132') }}">Share on LinkedIn</a>
                         </div>
 
