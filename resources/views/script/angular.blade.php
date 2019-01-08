0523e3331333e4fd2d232f6e93a9af7672045d80
Cindy Leschaud <cindy.leschaud@gmail.com>
2019-01-08 08:48:04 +0100

    re-add redactor angular for now

--- resources/views/script/angular.blade.php
+++ resources/views/script/angular.blade.php
@@ -1,3 +1,5 @@
 <script type="text/javascript" src="{{ secure_asset('newsletter/angular/angular.min.js') }}"></script>
 <script type="text/javascript" src="{{ secure_asset('newsletter/angular/angular-sanitize.js') }}"></script>
 <script type="text/javascript" src="{{ secure_asset('newsletter/angular/angular-flow.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('newsletter/angular/angular-redactor-2.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('newsletter/angular/angular-drag-and-drop-lists.js') }}"></script>
