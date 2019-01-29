50b7675323618086f05aa26c9d2797125479fd54
Cindy Leschaud <cindy.leschaud@gmail.com>
2019-01-29 10:20:52 +0100

    remove index on clone product row

--- resources/views/backend/orders/create.blade.php
+++ resources/views/backend/orders/create.blade.php
@@ -46,14 +46,14 @@
                                     $old_products  = Session::get('old_products');
                                     $first_product = array_shift($old_products);
                                 ?>
-                                @include('backend.orders.partials.product', ['id' => 'fieldset_clone_order', 'old_product' => $first_product, 'index' => 0])
+                                @include('backend.orders.partials.product', ['id' => 'fieldset_clone_order', 'old_product' => $first_product, 'index' => ''])
 
                                 @foreach($old_products as $index => $old_product)
                                     <?php $index = isset($index) ? $index + 1 : 1; ?>
                                     @include('backend.orders.partials.product', ['id' => '', 'old_product' => $old_product, 'index' => $index])
                                 @endforeach
                             @else
-                                @include('backend.orders.partials.product', ['id' => 'fieldset_clone_order', 'index' => 0])
+                                @include('backend.orders.partials.product', ['id' => 'fieldset_clone_order', 'index' => ''])
                             @endif
                         </div>
 
