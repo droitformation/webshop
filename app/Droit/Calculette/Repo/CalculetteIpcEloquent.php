b5788e6fa8b30d7552612cedc725cb2d43c44765
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-01-07 08:44:31 +0100

    add s to order calculette ipc

--- app/Droit/Calculette/Repo/CalculetteIpcEloquent.php
+++ app/Droit/Calculette/Repo/CalculetteIpcEloquent.php
@@ -14,7 +14,7 @@ class CalculetteIpcEloquent implements CalculetteIpcInterface{
 
 	public function getAll(){
 
-		return $this->calculette->orderBy('start_at','DES')->get();
+		return $this->calculette->orderBy('start_at','DESC')->get();
 	}
 
 	public function find($id){
