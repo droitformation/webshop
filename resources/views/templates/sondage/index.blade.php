7253893ad163fd167c8d67e76ef59dbe9ed5a093
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-06-03 11:20:51 +0200

    small fixes sondages pdf

--- resources/views/templates/sondage/index.blade.php
+++ resources/views/templates/sondage/index.blade.php
@@ -6,7 +6,16 @@
     <link rel="stylesheet" type="text/css" href="{{ secure_asset('common/css/generate/common.css') }}" media="screen" />
     <link rel="stylesheet" type="text/css" href="{{ secure_asset('common/css/generate/bon.css') }}" media="screen" />
     <link rel="stylesheet" type="text/css" href="{{ secure_asset('common/css/generate/invoice.css') }}" media="screen" />
+    <style>
+        *{
+            font-size: 13px;
+            line-height: 19px;
+            font-weight: 400;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
 </head>
+
 <body style="padding: 2mm; position: relative;">
 
 <div class="content">
@@ -63,17 +72,16 @@
         @if($sort == 'reponse_id')
             <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse; margin-bottom: 20px; display: table">
                 @foreach($reponses as $id => $response)
-
-                    <tr>
-                        <td><h4>{{ $response->first()->response->email }}</h4></td>
-                        <td>
-                            @foreach($response as $avis)
-                                <div>{!! $avis->avis->question !!}</div>
-                                <div>{!! $avis->reponse !!}</div>
+                    <tr valign="top">
+                        <td valign="top"><h4 style="font-weight: bold;margin: 0;">{{ $response->first()->response->email }}</h4></td>
+                        <td valign="top">
+                            @foreach($response->sortByDesc('avis_id') as $avis)
+                                <div style="margin-bottom:5px; font-weight: bold;font-size: 15px;">{!! strip_tags($avis->avis->question) !!}</div>
+                                <div>{!! strip_tags($avis->reponse) !!}</div>
                             @endforeach
                         </td>
                     </tr>
-
+                    <tr valign="top"><td>&nbsp;</td><td>&nbsp;</td></tr>
                 @endforeach
             </table>
         @endif
