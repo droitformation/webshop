47c3080318983c8ae7574d29f28a8ab898877ce1
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-06-29 09:01:47 +0200

    add preview book in master

--- resources/views/frontend/pubdroit/partials/preview.blade.php
+++ resources/views/frontend/pubdroit/partials/preview.blade.php
@@ -1,6 +1,7 @@
 <div id="adobe-dc-view" style="width: 100%; height: 600px;"></div>
 <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
 <script type="text/javascript">
+    // https://console.adobe.io/home
     document.addEventListener("adobe_dc_view_sdk.ready", function(){
         var adobeDCView = new AdobeDC.View({clientId: "f51f8e3bd64c4de386b4ac5e5122a348", divId: "adobe-dc-view",locale: "fr-FR"});
         adobeDCView.previewFile({
