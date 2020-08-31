d17a8e9617af59eb7631af5544c0c57e1acf5035
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-08-31 18:20:16 +0200

    css push load after maj

--- resources/views/frontend/pubdroit/layouts/master.blade.php
+++ resources/views/frontend/pubdroit/layouts/master.blade.php
@@ -33,7 +33,8 @@
 
 	<!-- All css -->
 	<link rel="stylesheet" href="<?php echo secure_asset('frontend/pubdroit/css/bs.css');?>">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">	<link rel="stylesheet" href="<?php echo secure_asset('frontend/pubdroit/css/style.css');?>">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo secure_asset('frontend/pubdroit/css/style.css');?>?{{ rand(100,43000) }}">
 
 	<!-- Css Files Start -->
 	<link rel="stylesheet" href="<?php echo secure_asset('frontend/pubdroit/css/skins/red.css');?>">
