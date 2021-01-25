1073eb534e6619b0cf513d8f38f0fe0eb6ac6803
Cindy Leschaud <cindy.leschaud@gmail.com>
2021-01-25 08:37:48 +0100

    changes local

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit4e9f05bf6c0c08726b822494c448369c
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit4e9f05bf6c0c08726b822494c448369c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit4e9f05bf6c0c08726b822494c448369c', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit4e9f05bf6c0c08726b822494c448369c::getInitializer($loader));
         } else {
